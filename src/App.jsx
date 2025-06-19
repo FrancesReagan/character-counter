f6a9e092752f7b8ed166404e4152025bd65664c3
Frances Reagan James <adtgwen@gmail.com>
2025-06-18 23:34:09 -0400

    added Toggler

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@
 // CharacterCounter and passes some configuration props (minWords, maxWords, targetReadingTime). App is like a simplewrapper that sets up the app but doesn't manage any
 // dynamic data//
 import './App.css'
-import CharacterCounter from './CharacterCounter';
+import CharacterCounter from './components/CharacterCounter';
+import Toggler from './components/Toggler';
 
 function App() {
   return (
     <div style={{ fontFamily: "Arial, sans-serif" }}>
   <CharacterCounter minWords={25} maxWords={100} targetReadingTime={1}/>
+    </div>
+    <div>
+      <Toggler />
     </div>
   );
 }
